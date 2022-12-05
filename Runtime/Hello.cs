b2e89de1b82784629e18cb7295aa07635ecaf81e
Sebastian Bularca <6408802+JovianIndustries@users.noreply.github.com>
2022-12-05 13:13:20 +0100

    Initial commit

---
+++ Runtime/Hello.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+namespace Fidelity.PackageTemplate {
+    /// <summary>
+    /// This useful class can be used to print 'Hello World' to the console.
+    /// </summary>
+    public static class Hello {
+        /// <summary>
+        /// Prints 'Hello World' to the console.
+        /// </summary>
+        public static void World() {
+            Debug.Log("Hello World");
+        }
+    }
+}
+
